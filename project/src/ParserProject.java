2f22b9f16a813416c456037352d79db1fa9d0d04
Olothando Masiza <olm6nt@umsystem.edu>
2024-12-08 13:56:46 -0600

    new read me and tree printing method added

--- project/src/ParserProject.java
+++ project/src/ParserProject.java
@@ -1,13 +1,15 @@
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.Trees;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 
-public class ParserProject{
-    public static void main (String[] args){
+public class ParserProject {
+    public static void main(String[] args) {
         try {
             String code = new String(Files.readAllBytes(Paths.get("test/project_deliverable_1.py")));
 
@@ -16,11 +18,30 @@ public class ParserProject{
             Python3Parser parser = new Python3Parser(tokens);
 
             // Parse the input code
-            ParseTree tree = parser.start(); // Start from the 'start' rule
+            ParseTree tree = parser.start();
 
+            // Print raw tree format
+            System.out.println("Parse Tree (LISP-style format):");
+            System.out.println(tree.toStringTree(parser));
+            
+            // Print pretty tree format
+            System.out.println("\nParse Tree (Pretty format):");
+            printTree(tree, 0, parser);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        } catch (Exception e) {
-            System.err.println("Error reading the file: " + e.getMessage());
+    private static void printTree(ParseTree tree, int level, Python3Parser parser) {
+        // Print indentation
+        System.out.print(String.join("", Collections.nCopies(level, "  ")));
+        
+        // Print node
+        System.out.println(Trees.getNodeText(tree, parser));
+        
+        // Print all children
+        for (int i = 0; i < tree.getChildCount(); i++) {
+            printTree(tree.getChild(i), level + 1, parser);
         }
     }
 }
